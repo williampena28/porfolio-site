51f97aba0c2439ae8defd4d2f8b180c1b31ff73a
williampena28 <56842242+williampena28@users.noreply.github.com>
2023-02-26 20:52:45 -0500

    Nav bar style

---
+++ src/components/nav/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.css'
+
+const Nav = () => {
+  return (
+    <div className='nav-bar'>
+        <nav className='nav-items'>
+            <p>About</p>
+            <p>Contact</p>
+            <p>Projects</p>
+            <p>Education</p>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/pages/main/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+const MainPage = () => {
+  return (
+    <div className='main-page-content'>
+        <h1>Hi, I'm William Peña</h1>
+        <p>I am a rising MERN full-stack web developer with experience in C++20, node.js, and data management. You can contact me @ `williampena2022@gmail.com`</p>
+    </div>
+  )
+}
+
+export default MainPage
