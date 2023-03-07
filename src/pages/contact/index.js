592636b137561d5014036914e6b5fda1ab29af1d
williampena28 <56842242+williampena28@users.noreply.github.com>
2023-03-07 14:36:23 -0500

    Main page position centered

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -8,6 +8,8 @@ const Contact = () => {
         <h3>Email: williampena2022@gmail.com</h3>
         <h3>Phone:(929)-368-1917</h3>
         <Link to='https://www.linkedin.com/in/william-pe%C3%B1a-32623115a/'><h3>Linkedin Page</h3></Link>
+        <Link to='https://github.com/williampena28'><h3>Github</h3></Link>
+        <Link to='https://docs.google.com/document/d/1AGMg_ZzYMNAlvdp1IKhGZbKBsq74YFig/edit?usp=sharing&ouid=116509990734666334770&rtpof=true&sd=true'><h3>Resume Doc</h3></Link>
       </div>
     </div>
   )
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,8 +4,12 @@ import './index.css'
 const MainPage = () => {
   return (
     <div className='main-page-content'>
+      <div className='content-wrap'>
         <h1>Hi, I'm William Peña</h1>
-        <p>I am a rising MERN full-stack web developer with experience in C++20, node.js, and data management. You can contact me @ `williampena2022@gmail.com`</p>
+        <p id='main-page-description'>I am a rising MERN full-stack web developer with experience in C++20, node.js, and data management.
+           You can contact me @ `williampena2022@gmail.com`
+        </p>
+      </div>
     </div>
   )
 }
